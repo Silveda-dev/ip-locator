f4a8e6247688520692e8c9734457b02b47f355d8
Silveda-dev <148528713+Silveda-dev@users.noreply.github.com>
2025-07-02 23:36:38 +0930

    Create write function reallocating memory to allow storage of pulled data

--- ip-locator.c
+++ ip-locator.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <curl/curl.h>
 #include <string.h>
 #include <regex.h>
+
+struct response_data {
+    char* response;
+    size_t size;
+};
 
 int ip_validator(const char* ip) {
     if (8 <= sizeof(ip)/sizeof(ip[0]) <= 16) { //Check IP length is within bounds
@@ -24,6 +30,22 @@ int ip_validator(const char* ip) {
         return -1;
 }
 
+size_t writefunc(char* data, size_t sz, size_t n, struct response_data* rd) {
+    size_t new_size = rd->size + sz * n;
+    rd->response = realloc(rd->response, new_size+1);
+
+    if (rd->response == NULL) {
+        fprintf(stderr, "realloc() error\n");
+        exit(1);
+    }
+
+    memcpy(rd->response + rd->size, data, sz*n);
+    rd->size = new_size;
+    rd->response[rd->size] = '\0';
+
+    return sz*n;
+}
+
 int main() {
     char full_addr[38] = "http://ip-api.com/json/";
     char ip_addr[20]; //Add additional space in case of error
@@ -35,15 +57,28 @@ int main() {
     if(ip_validator(ip_addr) != 0)
         return 0;
 
-    CURL* handle = curl_easy_init();
     //DEBUG LINE printf("Creating connection to %s\n", full_addr);
 
+    CURL* handle = curl_easy_init();
+
     if (handle) {
+        struct response_data rd;
+        rd.size = 0;
+        rd.response = calloc(1, sizeof(char));
+
         curl_easy_setopt(handle, CURLOPT_URL, full_addr);
+        curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, writefunc);
+        curl_easy_setopt(handle, CURLOPT_WRITEDATA, &rd);
+
         CURLcode ret_code = curl_easy_perform(handle);
 
-        if (ret_code != CURLE_OK)
+        if (ret_code != CURLE_OK) {
             fprintf(stderr, "curl connection failed. Error: %s\n", curl_easy_strerror(ret_code));
+            return 1;
+        }
+
+        printf("%s\n", rd.response);
+        free(rd.response);
 
         curl_easy_cleanup(handle);
     } else
