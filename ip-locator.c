e0357f99fd3486e00420d859d68dc255cc2f91ca
Silveda-dev <148528713+Silveda-dev@users.noreply.github.com>
2025-07-02 17:36:23 +0930

    Refactor code for API connection

--- ip-locator.c
+++ ip-locator.c
@@ -4,26 +4,21 @@
 
 int main() {
     char full_addr[38] = "http://ip-api.com/json/";
-    char ip_addr[16];
+    char ip_addr[20]; //Add additional space in case of error
+
     printf("Enter IP address: ");
     scanf("%s", &ip_addr);
-
-    curl_global_init(CURL_GLOBAL_ALL);
-    CURL* handle = curl_easy_init();
-    CURLcode ret_code;
-
     strcat(full_addr, ip_addr);
 
-    printf("Creating connection to %s\n", full_addr);
+    CURL* handle = curl_easy_init();
+    //DEBUG LINE printf("Creating connection to %s\n", full_addr);
 
     if (handle) {
         curl_easy_setopt(handle, CURLOPT_URL, full_addr);
-        ret_code = curl_easy_perform(handle);
+        CURLcode ret_code = curl_easy_perform(handle);
 
         if (ret_code != CURLE_OK)
-            fprintf(stderr, "curl failed. Error: %s\n", curl_easy_strerror(ret_code));
-        else
-            printf("Successful GET request\n");
+            fprintf(stderr, "curl connection failed. Error: %s\n", curl_easy_strerror(ret_code));
 
         curl_easy_cleanup(handle);
     } else
