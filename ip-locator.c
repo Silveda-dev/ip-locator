c14cb162ae2a2decf3aa556dc70b54fa96216b67
Silveda-dev <148528713+Silveda-dev@users.noreply.github.com>
2025-07-02 21:34:13 +0930

    Validate whether input is in correct IPv4 format

--- ip-locator.c
+++ ip-locator.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 #include <curl/curl.h>
 #include <string.h>
+#include <regex.h>
+
+int ip_validator(const char* ip) {
+    if (8 <= sizeof(ip)/sizeof(ip[0]) <= 16) { //Check IP length is within bounds
+        regex_t regex_var;
+        int regex_result = regcomp(&regex_var, "([0-9]{1,3}[.]){3}([0-9]{1,3})", REG_EXTENDED);
+
+        if (!regex_result) {
+            regex_result = regexec(&regex_var, ip, 0, NULL, 0);
+            if (!regex_result)
+                return 0; //Valid IP format
+            else {
+                fprintf(stderr, "Invalid IP format\n");
+                return -1;
+            }
+        } else {
+            fprintf(stderr, "REGEX compilation error\n");
+            return -1;
+        }
+    } else
+        return -1;
+}
 
 int main() {
     char full_addr[38] = "http://ip-api.com/json/";
@@ -9,6 +31,9 @@ int main() {
     printf("Enter IP address: ");
     scanf("%s", &ip_addr);
     strcat(full_addr, ip_addr);
+
+    if(ip_validator(ip_addr) != 0)
+        return 0;
 
     CURL* handle = curl_easy_init();
     //DEBUG LINE printf("Creating connection to %s\n", full_addr);
